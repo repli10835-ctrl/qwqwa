1f2948230144650fb28c395683814f9ad98d0d73
repli10835-ctrl <repli10835@gmail.com>
2025-10-11 01:40:38 +0700

    Updated request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,9 +1,7 @@
 import { getRequestConfig } from 'next-intl/server';
-import { headers } from 'next/headers';
 
 export default getRequestConfig(async () => {
-  const headersList = await headers();
-  const locale = headersList.get('x-locale') || 'id';
+  const locale = 'id';
 
   return {
     locale,
